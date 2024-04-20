61d80897962a05c1fb6dde3f495d7d0bca8d5d98
Aaron Barlow <abarlow505@gmail.com>
2024-04-20 12:59:50 -0400

    Fully tested

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-### Setup
+# Setup
+
+### Local
+
+1. virtualenv myenv
+
+### Prod
+
+1. Upload to glitch
 
 
-
-### Tools
+# Tools
 
 1. Glitch: glitch.com
+
+
+# Example Usage
+
+1. /oauth/callback?code=codetest&state=statetest
+    - providing state and code
+2. /get_code/statetest
+    - get code by state
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
-from flask import jsonify
+from waitress import serve
+import  jsonpickle
 import os
 
 PORT = 3000
+DEBUG = False
+DEVELOPMENT = False
 app = Flask(__name__)
 codes = {}
 
@@ -12,24 +15,27 @@ codes = {}
 # 3. Hit oauth_callback endpoint
 # 4. Hit get_code endpoint with the state to get the code
 
-@app.route('/startup')
+@app.route('/startup', methods=['GET'])
 def startup():
     # App will stay up for 5 minutes on glitch
-    return True
+    return jsonpickle.encode({ 'status': True })
 
 @app.route('/oauth/callback')
-def oauth_callback():
+def oauth_callback(methods=['POST']):
     auth_code = request.args.get('code')
     state = request.args.get('state')
     codes[state] = auth_code
-    return jsonify({ 'status': 'good' }), 200
+    return jsonpickle.encode({ 'status': 'good' })
 
-@app.route('/get_code/<state>')
+@app.route('/get_code/<state>', methods=['GET'])
 def get_code(state):
-    code = codes.get(state, None)
+    code = codes.get(state)
     if code:
-        return jsonify({ 'code': code })
-    return jsonify({ 'error': 'Code not found' }), 404
+        return jsonpickle.encode({ 'code': code })
+    return jsonpickle.encode({ 'error': 'Code not found' })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', PORT)))
+    if DEVELOPMENT:
+        app.run(debug=DEBUG, host='0.0.0.0', port=int(os.environ.get('PORT', PORT)))
+    else:
+        serve(app, host="0.0.0.0", port=PORT)
