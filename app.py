e8e6e5187c464708a9d123d0ac4b1d9ab5fcf236
Aaron Barlow <abarlow505@gmail.com>
2024-04-20 12:15:15 -0400

    Complete

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ def oauth_callback():
     auth_code = request.args.get('code')
     state = request.args.get('state')
     codes[state] = auth_code
-    return "Code received. You can close this page."
+    return { 'status': 'good' }, 200
 
 @app.route('/get_code/<state>')
 def get_code(state):
     code = codes.get(state, None)
     if code:
-        return {'code': code}
-    return {'error': 'Code not found'}, 404
+        return { 'code': code }
+    return { 'error': 'Code not found' }, 404
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', PORT)))
